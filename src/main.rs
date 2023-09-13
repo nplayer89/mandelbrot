499df4d29e2d02de00f134b1bea3e2a5659a531b
nplayer89 <n.player89@gmail.com>
2023-09-12 18:16:49 -0600

    run cargo fmt

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,8 @@ fn main() {
     println!("Hello, world!");
 }
 
-fn escape_time(c: Complex<f64>, limit usize) -> Option<usize>{
-    let mut z = Complex {re: 0.0, im: 0.0};
+fn escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
+    let mut z = Complex { re: 0.0, im: 0.0 };
     for i in 0..limit {
         if z.norm_sqr() > 4.0 {
             return Some(i);
@@ -17,53 +17,59 @@ fn escape_time(c: Complex<f64>, limit usize) -> Option<usize>{
     None
 }
 
-fn parse_pair<T: FromStr>(s: &str, separator:char) -> Option<(T, T)> {
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
     match s.find(separator) {
         None => None,
-        Some(index) => {
-            match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
-                (Ok(l), Ok(r)) => Some((l,r)),
-                _ => None
-            }
-        }
+        Some(index) => match (T::from_str(&s[..index]), T::from_str(&s[index + 1..])) {
+            (Ok(l), Ok(r)) => Some((l, r)),
+            _ => None,
+        },
     }
 }
 
 #[test]
 fn test_parse_pair() {
-    assert_eq!(parse_pair::<i32>("",    ','), None);
-    assert_eq!(parse_pair::<i32>("10,",    ','), None);
-    assert_eq!(parse_pair::<i32>(",10",    ','), None);
-    assert_eq!(parse_pair::<i32>("10,10",    ','), Some((10,20)));
-    assert_eq!(parse_pair::<i32>("10,20xy",    ','), None);
-    assert_eq!(parse_pair::<i32>("0.5x",    'x'), None);
-    assert_eq!(parse_pair::<i32>("0.5x1.5",    'x'), Some((0.5, 1.5)));
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10,", ','), None);
+    assert_eq!(parse_pair::<i32>(",10", ','), None);
+    assert_eq!(parse_pair::<i32>("10,10", ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<i32>("0.5x", 'x'), None);
+    assert_eq!(parse_pair::<i32>("0.5x1.5", 'x'), Some((0.5, 1.5)));
 }
 
 fn parse_complex(s: &str) -> Option<Complex<f64>> {
     match parse_pair(s, ',') {
-        Some((re, im)) => Some(Complex {re, im}),
-        None => None
+        Some((re, im)) => Some(Complex { re, im }),
+        None => None,
     }
 }
 
 #[test]
 fn test_parse_complex() {
-    assert_eq!(parse_complex("1.25,-0.0625"),
-        Some(Complex { re: 1.25, im: -0.0625}));
+    assert_eq!(
+        parse_complex("1.25,-0.0625"),
+        Some(Complex {
+            re: 1.25,
+            im: -0.0625
+        })
+    );
     assert_eq!(parse_complex(",-0.0625"), None);
 }
 
-fn pixel_to_point(bounds: (usize, usize),
-                  pixel: (usize, usize),
-                  upper_left: Complex<f64>,
-                  lower_right: Complex<f64>
-    -> Complex<f64>
-{
-    let (width, height) = (lower_right.re - upper_left.re, upper_left.im - lower_right.im);
-    Complex{
+fn pixel_to_point(
+    bounds: (usize, usize),
+    pixel: (usize, usize),
+    upper_left: Complex<f64>,
+    lower_right: Complex<f64>,
+) -> Complex<f64> {
+    let (width, height) = (
+        lower_right.re - upper_left.re,
+        upper_left.im - lower_right.im,
+    );
+    Complex {
         re: upper_left.re + pixel.0 as f64 * width / bounds.0 as f64,
-        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64
+        im: upper_left.im - pixel.1 as f64 * height / bounds.1 as f64,
     }
 }
 
