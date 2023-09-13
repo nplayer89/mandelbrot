cec9b8abeede2d66c0a39edcd38b4c9b06389cca
nplayer89 <n.player89@gmail.com>
2023-09-13 11:38:52 -0600

    add render function

--- src/main.rs
+++ src/main.rs
@@ -77,3 +77,18 @@ fn pixel_to_point(
 fn test_pixel_to_point() {
     assert_eq!(pixel_to_point((100, 200), (25, 175), Complex { re: -1.0, im: 1.0 }, Complex { re: 1.0, im: -1.0}), Complex { re: -0.5, im: -0.75});
 }
+
+
+fn render(pixels: &mut[u8], bounds: (usize,usize), upper_left: Complex<f64>, lower_right: Complex<f64>){
+    assert!(pixels.len() == bounds.0 * bounds.1);
+
+    for row in 0..bounds.1{
+        for column in 0..bounds.0 {
+            let point = pixel_to_point(bounds, (column,row), upper_left, lower_right);
+            pixels[row * bounds.0 + column] = match escape_time(point, 255) {
+                None => 0,
+                Some(count) => 255 - count as u8
+            };
+        }
+    }
+}
