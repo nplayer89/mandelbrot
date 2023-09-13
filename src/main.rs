4a722275268075e6f112a363ff94b29f7dbcc9a8
nplayer89 <49922233+nplayer89@users.noreply.github.com>
2023-09-12 18:19:07 -0600

    fix typos

--- src/main.rs
+++ src/main.rs
@@ -34,8 +34,8 @@ fn test_parse_pair() {
     assert_eq!(parse_pair::<i32>(",10", ','), None);
     assert_eq!(parse_pair::<i32>("10,10", ','), Some((10, 20)));
     assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
-    assert_eq!(parse_pair::<i32>("0.5x", 'x'), None);
-    assert_eq!(parse_pair::<i32>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+    assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
 }
 
 fn parse_complex(s: &str) -> Option<Complex<f64>> {
@@ -75,5 +75,5 @@ fn pixel_to_point(
 
 #[test]
 fn test_pixel_to_point() {
-    assert_eq!(pixel_to_point((100, 200), (25, 175), Complex { reL -1.0, im: 1.0 }, Complex { re: 1.0, im: -1.0}), Complex { re: -0.5, im: -0.75});
+    assert_eq!(pixel_to_point((100, 200), (25, 175), Complex { re: -1.0, im: 1.0 }, Complex { re: 1.0, im: -1.0}), Complex { re: -0.5, im: -0.75});
 }
